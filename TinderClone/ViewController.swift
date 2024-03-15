4e49bf43a6429e8b4b0a50fe99906a3f16af3508
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-03-15 21:00:23 +0300

    Created Extension.

--- TinderClone/ViewController.swift
+++ TinderClone/ViewController.swift
@@ -11,9 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        let grayView = UIView()
+        grayView.backgroundColor = .gray
+        
+        let redView = UIView()
+        redView.backgroundColor = .red
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        let stackView = UIStackView(arrangedSubviews: [redView, blueView])
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        stackView.frame = .init(x: 0, y: 0, width: 300, height: 200)
+        
+        stackView.fillSuperview()
+        
     }
 
 
 }
-
