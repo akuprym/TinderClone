9170834e8f41bba187f9a91df838c72a86cf6347
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-03-23 00:09:38 +0300

    Created CardView with ImageView.

---
+++ TinderClone/Views/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  TinderClone
+//
+//  Created by admin on 22.03.24.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
