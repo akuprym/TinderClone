f77ef2ed54b1ccebeff8b17875d5724a8c669dbd
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-03-22 22:53:34 +0300

    Created TopNavStackView.

---
+++ TinderClone/Views/TopNavigationStackView.swift
@@ -0,0 +1,20 @@
+//
+//  TopNavigationStackView.swift
+//  TinderClone
+//
+//  Created by admin on 21.03.24.
+//
+
+import UIKit
+
+class TopNavigationStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
