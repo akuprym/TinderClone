f226321c0b9df2314f9edfa00cb3afc20ee5633d
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-03-29 23:49:11 +0300

    Set up Gradient Layer for registration VC

---
+++ TinderClone/Views/CustomTextField.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTextField.swift
+//  TinderClone
+//
+//  Created by admin on 29.03.24.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
