3c8a71df74c64545059fca8b1ab434c5c4f99210
Anna Kuprym <annakupriyanchik99@gmail.com>
2024-04-05 22:44:20 +0300

    Created Registration View Model.

--- TinderClone/ViewModels/RegistrationViewModel.swift
+++ TinderClone/ViewModels/RegistrationViewModel.swift
@@ -9,8 +9,16 @@ import UIKit
 
 class RegistrationViewModel {
     
-    var fullName: String?
-    var email: String?
-    var password: String?
+    var fullName: String? { didSet { checkFormValidity() }}
+    var email: String? { didSet { checkFormValidity() }}
+    var password: String? { didSet { checkFormValidity() }}
     
+    fileprivate func checkFormValidity() {
+        let isFormValid = fullName?.isEmpty == false && email?.isEmpty == false && password?.isEmpty == false
+        isFormValidObserver?(isFormValid)
+    }
+    
+    // Reactive programming
+    
+    var isFormValidObserver: ((Bool) -> ())?
 }
